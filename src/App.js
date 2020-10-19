103874e270666529016cb2c0f5fab6870d96df07
Roberts <robertssteins@gmail.com>
2020-10-19 16:28:15 +0300

    something

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const value = 'value';
+  console.log({value});
+  localStorage.setItem('random_key', 'value');
   return (
     <div className="App">
       <header className="App-header">
